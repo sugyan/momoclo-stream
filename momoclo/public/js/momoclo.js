9a82b100e13fd2bfa1dd8dcd86b27bc5ff7c57b2
sugyan <sugi1982@gmail.com>
2011-08-16 09:26:07 +0900

    slideDown effect

--- momoclo/public/js/momoclo.js
+++ momoclo/public/js/momoclo.js
@@ -6,21 +6,21 @@ $(function () {
     });
     socket.on('tweet', function (data) {
         data.text = data.text.replace(/(http:\/\/t\.co\/\w{7})/g, '<a href="$1" target="_blank">$1</a>');
-        $('#tweets').prepend(
-            $('<div>').attr({ id: data.id }).addClass('tweet')
-                .append($('<div>').addClass('image')
-                        .append($('<img>').attr({ src: data.icon })))
-                .append($('<div>').addClass('text')
-                        .append($('<div>').addClass('info')
-                                .append($('<span>').text('@' + data.user))
-                                .append($('<span>').addClass('date')
-                                        .append($('<a>').attr({
-                                            href: 'https://twitter.com/#!/' + data.user + '/status/' + data.id,
+        var tweet = $('<div>').attr({ id: data.id }).addClass('tweet').hide()
+            .append($('<div>').addClass('image')
+                    .append($('<img>').attr({ src: data.icon })))
+            .append($('<div>').addClass('text')
+                    .append($('<div>').addClass('info')
+                            .append($('<span>').text('@' + data.user))
+                            .append($('<span>').addClass('date')
+                                    .append($('<a>').attr({
+                                        href: 'https://twitter.com/#!/' + data.user + '/status/' + data.id,
                                             target: '_blank'
-                                        }).text(new Date(data.date).toLocaleString()))))
-                        .append($('<div>')
-                                .append($('<span>').html(data.text))))
-        );
+                                    }).text(new Date(data.date).toLocaleString()))))
+                    .append($('<div>')
+                            .append($('<span>').html(data.text))));
+        $('#tweets').prepend(tweet);
+        tweet.slideDown();
         // anywhere
         twttr.anywhere(function (T) {
             T("#" + data.id).linkifyUsers();
